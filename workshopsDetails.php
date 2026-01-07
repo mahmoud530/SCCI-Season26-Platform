abed6bb34eb15dbc2662138b75e57bf5c768897e
AwadCoding <112626336+AwadCoding@users.noreply.github.com>
2026-01-07 23:47:09 +0200

    edit image

--- workshopsDetails.php
+++ workshopsDetails.php
@@ -60,7 +60,7 @@ if (isset($_GET['category_id'])) {
                 </div>
 
                 <a href="workshops.php" class="backBtn">
-                    <i class="fas fa-arrow-left"></i> Back
+                    <i class="fas fa-arrow-left"></i>
                 </a>
 
                 <div class="workshopDescription">
